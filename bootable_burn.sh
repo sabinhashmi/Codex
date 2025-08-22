656ab4ed8c011d87a8c776f23244a56cd3e05710
Sabin Hashmi <61133611+sabinhashmi@users.noreply.github.com>
2025-08-22 15:39:53 +0200

    Update bootable_burn.sh

--- bootable_burn.sh
+++ bootable_burn.sh
@@ -1,5 +1,5 @@
 # Convert the image
-hdiutil convert -format UDRW -o <New.dmg> <Original.iso>
+hdiutil convert -format UDRW -o <New.dmg> <Original.iso> [SKIP for FEDORA]
 
 #Find disk name
 diskutil list
@@ -11,6 +11,8 @@ diskutil unmountDisk /dev/diskX
 
 # Write to rdiskX with raw access
 sudo dd if=New.dmg of=/dev/rdiskX bs=1m\n
+
+sudo dd if=~/Downloads/Fedora.iso of=/dev/rdisk2 bs=4m status=progress [For FEDORA]
 
 # Eject the disk
 diskutil eject /dev/diskX
